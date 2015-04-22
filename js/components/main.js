ce1a26b8b62336c7dcff5dceea479e4f47df966c
Stephen Grider <ste.grider@gmail.com>
2015-04-22 13:28:28 -0700

    added basic routing

---
+++ js/components/main.js
@@ -0,0 +1,114 @@
+var React = require('react-native');
+var Api = require('../utils/api');
+var {
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  TouchableHighlight,
+  ActivityIndicatorIOS
+} = React;
+
+var styles = StyleSheet.create({
+  mainContainer: {
+    flex: 1,
+    padding: 30,
+    marginTop: 65,
+    flexDirection: 'column',
+    justifyContent: 'center',
+    backgroundColor: '#48BBEC'
+  },
+  title: {
+    marginBottom: 20,
+    fontSize: 25,
+    textAlign: 'center',
+    color: '#fff'
+  },
+  searchInput: {
+    height: 50,
+    padding: 4,
+    marginRight: 5,
+    fontSize: 23,
+    borderWidth: 1,
+    borderColor: 'white',
+    borderRadius: 8,
+    color: 'white'
+  },
+  buttonText: {
+    fontSize: 18,
+    color: '#111',
+    alignSelf: 'center'
+  },
+  button: {
+    height: 45,
+    flexDirection: 'row',
+    backgroundColor: 'white',
+    borderColor: 'white',
+    borderWidth: 1,
+    borderRadius: 8,
+    marginBottom: 10,
+    marginTop: 10,
+    alignSelf: 'stretch',
+    justifyContent: 'center'
+  },
+});
+
+class Main extends React.Component{
+  constructor (props) {
+    super(props);
+    this.getInitialState()
+  }
+  getInitialState () {
+    this.state = {
+      username: '',
+      isLoading: false,
+      error: false
+    };
+  }
+  handleChange (event) {
+    this.setState({
+      username: event.nativeEvent.text
+    })
+  }
+  handlePress (event) {
+    this.setState({
+      isLoading: true,
+      username: ''
+    });
+
+    Api.getBio(this.state.username)
+      .then((res) => {
+        debugger
+        if(res.message === 'Not Found') {
+          this.setState({
+            error: 'Not Found'
+          })
+        } else {
+          this.props.navigator.push({
+            title: res.name || 'Select an Option',
+            component: Dashboard,
+            passProps: {userInfo: res}
+          });
+          this.getInitialState();
+        }
+      });
+  }
+  render () {
+    return (
+      <View style={styles.mainContainer}>
+        <Text style={styles.title}>Search for a Github User</Text>
+        <TextInput
+          style={styles.searchInput}
+          value={this.state.username}
+          onChange={this.handleChange.bind(this)} />
+        <TouchableHighlight style={styles.button}
+          onPress={this.handlePress.bind(this)}
+          underlayColor="white">
+          <Text style={styles.buttonText}> SEARCH </Text>
+        </TouchableHighlight>
+      </View>
+    )
+  }
+}
+
+module.exports = Main;
